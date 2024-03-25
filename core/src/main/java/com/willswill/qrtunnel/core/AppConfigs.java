959f62d65bd19b0e7e232cf45ef1fb0eb8efde1c
TomXin <766781886@qq.com>
2024-03-25 15:29:49 +0800

    优化

--- core/src/main/java/com/willswill/qrtunnel/core/AppConfigs.java
+++ core/src/main/java/com/willswill/qrtunnel/core/AppConfigs.java
@@ -10,8 +10,8 @@ public class AppConfigs {
     private int chunkSize = 1000; // 每张图片所包含的数据大小，最大值是
     private int sendInterval = 200; // 发送端每张图片停顿时间（毫秒）
 
-    private int imageWidth = 400; // 生成的图片宽度
-    private int imageHeight = 400; // 生成的图片高度
+    private int imageWidth = 350; // 生成的图片宽度
+    private int imageHeight = 350; // 生成的图片高度
 
     private String senderLayout; // 发送端图片布局
 
